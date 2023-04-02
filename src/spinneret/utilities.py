b8d6d05e144fd5c9178ab04eaad437487856f930
Colin Smith <colin.smith@wisc.edu>
2023-04-02 13:28:20 -0700

    Add utilities module for multi-purpose functions

---
+++ src/spinneret/utilities.py
@@ -0,0 +1,15 @@
+"""Utilities for the spinneret package"""
+
+
+def user_agent():
+    """Define the spinneret user agent in HTTP requests
+
+    For use with the `header` parameter of the requests library.
+
+    Returns
+    -------
+    dict
+        User agent
+    """
+    header = {'user-agent': 'spinneret Python package'}
+    return header
