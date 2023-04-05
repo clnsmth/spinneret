322f340eb2e3da3785baccfb7f805c668f05bda9
Colin Smith <colin.smith@wisc.edu>
2023-04-05 16:07:24 -0700

    Assemble preliminary ecosystem lookup workflow

--- src/spinneret/globalelu.py
+++ src/spinneret/globalelu.py
@@ -1,5 +1,10 @@
 """Get ENVO Classifications from Global Ecological Land Units Lookup"""
 import requests
+from spinneret.utilities import user_agent
+import glob
+import os.path
+import pickle
+from spinneret.eml import get_geographic_coverage
 
 
 def json_extract(obj, key):
@@ -23,7 +28,7 @@ def json_extract(obj, key):
     return values
 
 
-def identify(geometry=str, map_server=str, attributes=list):
+def identify(geometry=str, geometry_type=str, map_server=str):
     """
     Run an identify operation on a USGS map service resource and return the
     requested attributes
@@ -32,13 +37,18 @@ def identify(geometry=str, map_server=str, attributes=list):
 
     Parameters
     ----------
-    geometry
-    map_server
-    attributes
+    geometry : str
+        An ESRI geometry in JSON format.
+        for more information.
+    geometry_type : str
+        The `geometryType` parameter corresponding to `geometry`.
+    map_server : str
+        The map server to query.
 
     Returns
     -------
-
+    response : dict
+        A dictionary of the JSON response from the identify operation.
     """
     base = (
         "https://rmgsc.cr.usgs.gov/arcgis/rest/services/"
@@ -48,25 +58,60 @@ def identify(geometry=str, map_server=str, attributes=list):
     payload = {
         "f": "json",
         "geometry": geometry,
-        "geometryType": "esriGeometryPoint",
+        "geometryType": geometry_type,
         "tolerance": 2,
         "mapExtent": "-2.865, 47.628, 5.321, 50.017",
-        "imageDisplay": "600,550,96",
+        "imageDisplay": "600,550,96"
     }
-    r = requests.get(base, params=payload, timeout=5)
-    res = []
-    for attribute in attributes:
-        names = json_extract(r.json(), attribute)
-        res.append(names.pop())
-    # TODO Get SSSOM
-    # TODO Table lookup
+    r = requests.get(base, params=payload, timeout=5, headers=user_agent())
+    return r.json()
+
+
+def parse_attributes(attributes, json):
+    """Parse attributes from JSON response"""
+    res = {}
+    for a in attributes:
+        res[a] = json_extract(json, a)
     return res
 
 
+def annotate_eml(eml_dir, output_dir):
+    """Annotate EML metadata with USGS Global Ecosystem Lookup Service"""
+    res = []
+    files = glob.glob(eml_dir + '*.xml')
+    for file in files:
+        geocov = get_geographic_coverage(file)
+        if geocov is None:
+            continue  # TODO return "no geographic coverage"
+        for g in geocov:
+            if g._geom_type() == 'envelope' or g._geom_type() == 'polygon':
+                continue
+            geom = g.to_esri_geometry()
+            try:
+                r = identify(
+                    geometry=geom,
+                    geometry_type=g._geom_type(schema='esri'),
+                    map_server='wte'
+                )
+            except Exception as e:
+                print(e)
+                r = None # TODO return error message for logging
+            attributes = parse_attributes(
+                attributes=["Landforms", "Landcover", "Climate_Re"],
+                json=r
+            )
+            attributes['packageId'] = os.path.splitext(os.path.basename(file))[0]
+            attributes['geographicDescription'] = g.geographicDescription()
+            res.append(attributes)
+    return res
+
+
+
+
 if __name__ == "__main__":
-    res = identify(
-        geometry="-1.361,49.004",
-        map_server="wte",
-        attributes=["Landforms", "Landcover", "Climate_Re"],
+    res = annotate_eml(
+        eml_dir='/Users/csmith/Data/edi/eml/',
+        output_dir='/Users/csmith/Data/edi/tests/'
     )
-    print(res)
+    with open('/Users/csmith/Data/edi/tests/res.pkl', 'wb') as f:
+        pickle.dump(res, f)
