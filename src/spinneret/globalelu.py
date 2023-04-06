6efd1be8d780701a0f3e72c41ff88b01e462aa62
Colin Smith <colin.smith@wisc.edu>
2023-04-06 05:47:51 -0700

    Parse wte results and write to file

--- src/spinneret/globalelu.py
+++ src/spinneret/globalelu.py
@@ -5,6 +5,7 @@ import glob
 import os.path
 import pickle
 from spinneret.eml import get_geographic_coverage
+import pandas as pd
 
 
 def json_extract(obj, key):
@@ -75,11 +76,14 @@ def parse_attributes(attributes, json):
     return res
 
 
-def annotate_eml(eml_dir, output_dir):
-    """Annotate EML metadata with USGS Global Ecosystem Lookup Service"""
-    res = []
+def convert_geographic_coverage(eml_dir, output_dir):
+    """Convert EML geographicCoverage to ecosystem with USGS Global Ecosystem
+    Lookup Service"""
     files = glob.glob(eml_dir + '*.xml')
     for file in files:
+        res = []
+        print(file)
+        pkgid = os.path.splitext(os.path.basename(file))[0]
         geocov = get_geographic_coverage(file)
         if geocov is None:
             continue  # TODO return "no geographic coverage"
@@ -100,18 +104,53 @@ def annotate_eml(eml_dir, output_dir):
                 attributes=["Landforms", "Landcover", "Climate_Re"],
                 json=r
             )
-            attributes['packageId'] = os.path.splitext(os.path.basename(file))[0]
+            attributes['packageId'] = pkgid
             attributes['geographicDescription'] = g.geographicDescription()
             res.append(attributes)
-    return res
+        with open(output_dir + pkgid + '.pkl', 'wb') as f:
+            pickle.dump(res, f)
+    return None
+
+
+def combine_pkl_files(pkl_dir, output_dir):
+    """Combine pickle files into into a single dataframe"""
+    files = glob.glob(pkl_dir + '*.pkl')
+    res = []
+    for file in files:
+        with open(file, 'rb') as f:
+            res.append(pickle.load(f))
+    res_flat = [item for sublist in res for item in sublist] # FIXME pop() json_extract() step in parse attributes
+    # TODO cleanup code below
+    df = pd.DataFrame(res_flat)
+    df = df.explode('Landforms')
+    df = df.explode('Landcover')
+    df = df.explode('Climate_Re')
+    # Split packageId on first '.' to get datasetId
+    df['datasetId'] = df['packageId'].str.split('.', expand=True)[0]
+    df['datasetNum'] = df['packageId'].str.split('.', expand=True)[1] + '.' + df['packageId'].str.split('.', expand=True)[2]
+    df['datasetNum'] = df['datasetNum'].astype(float)
+    # Sort by datasetId and datasetNum
+    df = df.sort_values(by=['datasetId', 'datasetNum'])
+    # Drop last 3 columns of dataframe
+    df = df.iloc[:, :-2]
+    # Move packageId to front of dataframe
+    cols = df.columns.tolist()
+    cols = ['packageId'] + cols[:3] + cols[4:]
+    df = df[cols]
+    # Write df to tsv
+    df.to_csv(output_dir + 'globalelu.tsv', sep='\t', index=False)
+    return df
 
 
 
 
 if __name__ == "__main__":
-    res = annotate_eml(
-        eml_dir='/Users/csmith/Data/edi/eml/',
-        output_dir='/Users/csmith/Data/edi/tests/'
+    # res = convert_geographic_coverage(
+    #     eml_dir='/Users/csmith/Data/edi/eml/',
+    #     output_dir='/Users/csmith/Data/edi/tests/'
+    # )
+
+    res = combine_pkl_files(
+        pkl_dir='/Users/csmith/Data/edi/tests/',
+        output_dir='/Users/csmith/Data/edi/'
     )
-    with open('/Users/csmith/Data/edi/tests/res.pkl', 'wb') as f:
-        pickle.dump(res, f)
